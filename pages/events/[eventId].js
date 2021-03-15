d7e2bc41635915814c4c8f1beb90b2a0751e00f2
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro.local>
2021-03-15 16:52:17 +0300

    next refactoring and optimization

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,28 +1,26 @@
-
-import {useRouter} from 'next/router';
 import { Fragment } from 'react';
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../components/event-detail/event-summary';
-import EventsLogistics from '../components/event-detail/event-logistics';
-import EventContent from '../components/event-detail/event-content';
 
-const EventDetailPage = () => {
-  const router = useRouter();
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
 
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
-    return <p>No event found!!</p>;
+    return (
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
+    );
   }
-
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventsLogistics
+      <EventLogistics
         date={event.date}
         address={event.location}
         image={event.image}
@@ -33,6 +31,30 @@ const EventDetailPage = () => {
       </EventContent>
     </Fragment>
   );
-};
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: 'blocking',
+  };
+}
 
 export default EventDetailPage;
